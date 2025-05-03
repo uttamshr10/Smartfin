1a8977bf5862c9bd6ad5530d13c27c0a6d213388
Uttam <utam.shrestha65@gmail.com>
2025-05-03 18:54:28 +0545

    Add: Transaction API

---
+++ models/Transaction.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["income", "expense"],
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  note: String,
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/transactions", require("./routes/transactions"));
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI)
